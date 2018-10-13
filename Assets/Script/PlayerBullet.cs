cdd8a3fab77ffdda710c592dae414d5d605a584b
Masaya <n.masa.0901@gmail.com>
2018-10-14 00:24:05 +0900

    プレイヤースクリプト更新・弾の実装開始

---
+++ Assets/Script/PlayerBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+	public float speed; //速度
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+	}
+}
--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-	public float speed; //速度
+	public float maxspeed; //速度
+	public float lowspeed; //低速移動速度
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,21 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y + Input.GetAxis("Vertical") * speed, 0);
+		Move();
+	}
+
+	//移動用関数
+	void Move(){
+		float MoveX =  Input.GetAxisRaw("Horizontal");
+		float MoveY = Input.GetAxisRaw("Vertical");
+		Vector2 Dir = new Vector2(MoveX, MoveY).normalized;
+		float MoveSpeed;
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+			MoveSpeed = lowspeed;
+		}else{
+			MoveSpeed = maxspeed;
+		}
+		GetComponent<Rigidbody2D>().velocity = Dir * MoveSpeed;
 	}
 }
